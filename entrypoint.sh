9caaab5b1682b7082e334e38acbee7ba2c003379
Falk Pietsch <falk.pietsch@googlemail.com>
2019-11-29 14:15:03 +0100

    fix: echoing and grepping

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ function main() {
   DOCKERNAME="${INPUT_NAME}:${TAG}"
 
   # check if we should do anything at all with this branch
-  if { [ -z ${PUSH_BRANCH_TO_DOCKERHUB} ] || [ "${PUSH_BRANCH_TO_DOCKERHUB}" = "false" ]; } && [ "${TAG}" != "develop" ] && [ "${TAG}" != "master" ] && echo "${TAG}" | grep "release-"; then
+  if { [ -z ${PUSH_BRANCH_TO_DOCKERHUB} ] || [ "${PUSH_BRANCH_TO_DOCKERHUB}" = "false" ]; } && [ "${TAG}" != "develop" ] && [ "${TAG}" != "master" ]; then
     echo "workflow environment PUSH_BRANCH_TO_DOCKERHUB is false or not set and this is no default branch -> stopping push gracefully -> no error"
     exit 0;
   fi
