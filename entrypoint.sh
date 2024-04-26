25130b4e867a4773b4915c33d5b8cde0c1b57530
Ignacio Heredia <ignacio.nh@gmail.com>
2024-04-26 09:20:54 +0200

    Fix substitution

--- entrypoint.sh
+++ entrypoint.sh
@@ -77,7 +77,8 @@ function translateDockerTag() {
   elif isGitTag; then
     TAG="latest"
   elif isPullRequest; then
-    TAG="${{ github.head_ref }}"
+    BRANCH=$(echo ${{ github.head_ref }} | sed -e "s/refs\/heads\///g" | sed -e "s/\//-/g" | sed -e 's/[^ -~]//g')
+    TAG="${BRANCH}"
   elif isReleaseBranch; then
     TAG=$(echo "${GITHUB_REF}-rc" | sed -e "s/refs\/heads\/release\///g")
   else
